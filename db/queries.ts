dd57532f2d6582da609cb6b16b1cccc51cef2981
Mohamed Mbarkiou <mbarkioumed@outlook.com>
2025-05-24 18:22:00 +0100

    ss

--- db/queries.ts
+++ db/queries.ts
@@ -14,129 +14,136 @@ let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
 let db = drizzle(client);
 
 export async function getUser(email: string): Promise<Array<User>> {
-  try {
-    return await db.select().from(user).where(eq(user.email, email));
-  } catch (error) {
-    console.error("Failed to get user from database");
-    throw error;
-  }
+    try {
+        return await db.select().from(user).where(eq(user.email, email));
+    } catch (error) {
+        console.error("Failed to get user from database");
+        throw error;
+    }
 }
 
 export async function createUser(email: string, password: string) {
-  let salt = genSaltSync(10);
-  let hash = hashSync(password, salt);
+    let salt = genSaltSync(10);
+    let hash = hashSync(password, salt);
 
-  try {
-    return await db.insert(user).values({ email, password: hash });
-  } catch (error) {
-    console.error("Failed to create user in database");
-    throw error;
-  }
+    try {
+        return await db.insert(user).values({ email, password: hash });
+    } catch (error) {
+        console.error("Failed to create user in database");
+        throw error;
+    }
 }
 
 export async function saveChat({
-  id,
-  messages,
-  userId,
+    id,
+    messages,
+    userId,
 }: {
-  id: string;
-  messages: any;
-  userId: string;
+    id: string;
+    messages: any;
+    userId: string;
 }) {
-  try {
-    const selectedChats = await db.select().from(chat).where(eq(chat.id, id));
+    try {
+        const selectedChats = await db
+            .select()
+            .from(chat)
+            .where(eq(chat.id, id));
 
-    if (selectedChats.length > 0) {
-      return await db
-        .update(chat)
-        .set({
-          messages: JSON.stringify(messages),
-        })
-        .where(eq(chat.id, id));
+        if (selectedChats.length > 0) {
+            return await db
+                .update(chat)
+                .set({
+                    messages: JSON.stringify(messages),
+                })
+                .where(eq(chat.id, id));
+        }
+
+        return await db.insert(chat).values({
+            id,
+            createdAt: new Date(),
+            messages: JSON.stringify(messages),
+            userId,
+        });
+    } catch (error) {
+        console.error(error);
+        console.error("Failed to save chat in database");
+        throw error;
     }
-
-    return await db.insert(chat).values({
-      id,
-      createdAt: new Date(),
-      messages: JSON.stringify(messages),
-      userId,
-    });
-  } catch (error) {
-    console.error("Failed to save chat in database");
-    throw error;
-  }
 }
 
 export async function deleteChatById({ id }: { id: string }) {
-  try {
-    return await db.delete(chat).where(eq(chat.id, id));
-  } catch (error) {
-    console.error("Failed to delete chat by id from database");
-    throw error;
-  }
+    try {
+        return await db.delete(chat).where(eq(chat.id, id));
+    } catch (error) {
+        console.error("Failed to delete chat by id from database");
+        throw error;
+    }
 }
 
 export async function getChatsByUserId({ id }: { id: string }) {
-  try {
-    return await db
-      .select()
-      .from(chat)
-      .where(eq(chat.userId, id))
-      .orderBy(desc(chat.createdAt));
-  } catch (error) {
-    console.error("Failed to get chats by user from database");
-    throw error;
-  }
+    try {
+        return await db
+            .select()
+            .from(chat)
+            .where(eq(chat.userId, id))
+            .orderBy(desc(chat.createdAt));
+    } catch (error) {
+        console.error("Failed to get chats by user from database");
+        throw error;
+    }
 }
 
 export async function getChatById({ id }: { id: string }) {
-  try {
-    const [selectedChat] = await db.select().from(chat).where(eq(chat.id, id));
-    return selectedChat;
-  } catch (error) {
-    console.error("Failed to get chat by id from database");
-    throw error;
-  }
+    try {
+        const [selectedChat] = await db
+            .select()
+            .from(chat)
+            .where(eq(chat.id, id));
+        return selectedChat;
+    } catch (error) {
+        console.error("Failed to get chat by id from database");
+        throw error;
+    }
 }
 
 export async function createReservation({
-  id,
-  userId,
-  details,
-}: {
-  id: string;
-  userId: string;
-  details: any;
-}) {
-  return await db.insert(reservation).values({
     id,
-    createdAt: new Date(),
     userId,
-    hasCompletedPayment: false,
-    details: JSON.stringify(details),
-  });
+    details,
+}: {
+    id: string;
+    userId: string;
+    details: any;
+}) {
+    return await db.insert(reservation).values({
+        id,
+        createdAt: new Date(),
+        userId,
+        hasCompletedPayment: false,
+        details: JSON.stringify(details),
+    });
 }
 
 export async function getReservationById({ id }: { id: string }) {
-  const [selectedReservation] = await db
-    .select()
-    .from(reservation)
-    .where(eq(reservation.id, id));
+    const [selectedReservation] = await db
+        .select()
+        .from(reservation)
+        .where(eq(reservation.id, id));
 
-  return selectedReservation;
+    return selectedReservation;
 }
 
 export async function updateReservation({
-  id,
-  hasCompletedPayment,
+    id,
+    hasCompletedPayment,
 }: {
-  id: string;
-  hasCompletedPayment: boolean;
+    id: string;
+    hasCompletedPayment: boolean;
 }) {
-  return await db
-    .update(reservation)
-    .set({
-      hasCompletedPayment,
-    })
-    .where(eq(reservation.id, id));
+    return await db
+        .update(reservation)
+        .set({
+            hasCompletedPayment,
+        })
+        .where(eq(reservation.id, id));
 }
